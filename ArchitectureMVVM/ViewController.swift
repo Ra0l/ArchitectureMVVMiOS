62224edeb326fbb7a1fe3ad581fbf49f6ed8f983
Raul Kevin Aliaga Shapiama <45242019+Ra0l@users.noreply.github.com>
2024-03-22 09:17:31 -0500

    Initial Commit

---
+++ ArchitectureMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitectureMVVM
+//
+//  Created by Raul Kevin Aliaga Shapiama on 3/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
